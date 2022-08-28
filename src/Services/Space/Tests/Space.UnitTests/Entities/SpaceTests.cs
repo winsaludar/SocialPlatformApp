d7ecfc28f70cd88eb10153d2af5f58576bcf4cfc
winsaludar <erwinsaludar@gmail.com>
2022-08-28 16:54:38 +0800

    Added unit tests for space entity's create topic method

--- src/Services/Space/Tests/Space.UnitTests/Entities/SpaceTests.cs
+++ src/Services/Space/Tests/Space.UnitTests/Entities/SpaceTests.cs
@@ -1,6 +1,7 @@
 ﻿using Moq;
 using Space.Domain.Entities;
 using Space.Domain.Exceptions;
+using Space.Domain.Helpers;
 using Space.Domain.Repositories;
 using DomainEntities = Space.Domain.Entities;
 
@@ -9,6 +10,8 @@ namespace Space.UnitTests.Entities;
 public class SpaceTests
 {
     private readonly Mock<IRepositoryManager> _mockRepo;
+    private readonly Mock<IHelperManager> _mockHelper;
+    private readonly Mock<ISlugHelper> _mockSlugHelper;
 
     public SpaceTests()
     {
@@ -19,6 +22,10 @@ public class SpaceTests
         _mockRepo.Setup(x => x.UnitOfWork).Returns(mockUnitOfWork.Object);
         _mockRepo.Setup(x => x.SpaceRepository).Returns(mockSpaceRepo.Object);
         _mockRepo.Setup(x => x.SoulRepository).Returns(mockSoulRepo.Object);
+
+        _mockSlugHelper = new Mock<ISlugHelper>();
+        _mockHelper = new Mock<IHelperManager>();
+        _mockHelper.Setup(x => x.SlugHelper).Returns(_mockSlugHelper.Object);
     }
 
     [Theory]
@@ -97,5 +104,107 @@ public class SpaceTests
 
         _mockRepo.Verify(x => x.SoulRepository.DeleteSoulSpaceAsync(existingSoul.Id, space.Id), Times.Once);
         _mockRepo.Verify(x => x.UnitOfWork.CommitAsync(), Times.Once);
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData(null)]
+    public async Task CreateTopicAsync_AuthorEmailIsInvalid_ThrowsInvalidSoulException(string authorEmail)
+    {
+        DomainEntities.Space space = new() { };
+        string title = "Fake Title";
+        string content = "Fake Content";
+
+        await Assert.ThrowsAsync<InvalidSoulException>(() => space.CreateTopicAsync(authorEmail, title, content, _mockRepo.Object, _mockHelper.Object));
+    }
+
+    [Fact]
+    public async Task CreateTopicAsync_SpaceIsInvalid_ThrowsInvalidSpaceIdException()
+    {
+        DomainEntities.Space space = new() { Id = Guid.NewGuid() };
+        string authorEmail = "author@example.com";
+        string title = "Fake Title";
+        string content = "Fake Content";
+
+        _mockRepo.Setup(x => x.SpaceRepository.GetByIdAsync(It.IsAny<Guid>(), It.IsAny<bool>()))
+            .ReturnsAsync((DomainEntities.Space)null!);
+
+        await Assert.ThrowsAsync<InvalidSpaceIdException>(() => space.CreateTopicAsync(authorEmail, title, content, _mockRepo.Object, _mockHelper.Object));
+    }
+
+    [Fact]
+    public async Task CreateTopicAsync_SoulDoesNotExist_ThrowsInvalidSoulException()
+    {
+        DomainEntities.Space space = new() { Id = Guid.NewGuid() };
+        string authorEmail = "notexisting@example.com";
+        string title = "Fake Title";
+        string content = "Fake Content";
+
+        _mockRepo.Setup(x => x.SpaceRepository.GetByIdAsync(It.IsAny<Guid>(), It.IsAny<bool>()))
+            .ReturnsAsync(new DomainEntities.Space());
+        _mockRepo.Setup(x => x.SoulRepository.GetByEmailAsync(It.IsAny<string>(), It.IsAny<bool>()))
+            .ReturnsAsync((Soul)null!);
+
+        await Assert.ThrowsAsync<InvalidSoulException>(() => space.CreateTopicAsync(authorEmail, title, content, _mockRepo.Object, _mockHelper.Object));
+    }
+
+    [Fact]
+    public async Task CreateTopicAsync_SoulIsNotAMemberOfTheSpace_ThrowsSoulNotMemberException()
+    {
+        DomainEntities.Space space = new() { Id = Guid.NewGuid() };
+        string authorEmail = "notmember@example.com";
+        string title = "Fake Title";
+        string content = "Fake Content";
+
+        _mockRepo.Setup(x => x.SpaceRepository.GetByIdAsync(It.IsAny<Guid>(), It.IsAny<bool>()))
+            .ReturnsAsync(new DomainEntities.Space());
+        _mockRepo.Setup(x => x.SoulRepository.GetByEmailAsync(It.IsAny<string>(), It.IsAny<bool>()))
+            .ReturnsAsync(new Soul());
+        _mockRepo.Setup(x => x.SoulRepository.IsMemberOfSpaceAsync(It.IsAny<Guid>(), It.IsAny<Guid>()))
+            .ReturnsAsync(false);
+
+        await Assert.ThrowsAsync<SoulNotMemberException>(() => space.CreateTopicAsync(authorEmail, title, content, _mockRepo.Object, _mockHelper.Object));
+    }
+
+    [Fact]
+    public async Task CreateTopicAsync_SoulAndSpaceAreBothValidAndSoulIsAMember_CreateTopic()
+    {
+        Topic? createdTopic = null;
+        DomainEntities.Space space = new() { Id = Guid.NewGuid() };
+        string authorEmail = "member@example.com";
+        Topic newTopic = new(_mockHelper.Object)
+        {
+            Title = "Fake Title",
+            Content = "Fake Content",
+            SpaceId = space.Id,
+            SoulId = Guid.NewGuid(),
+            CreatedBy = authorEmail,
+            CreatedDateUtc = DateTime.UtcNow
+        };
+
+        _mockRepo.Setup(x => x.SpaceRepository.GetByIdAsync(It.IsAny<Guid>(), It.IsAny<bool>()))
+            .ReturnsAsync(new DomainEntities.Space());
+        _mockRepo.Setup(x => x.SoulRepository.GetByEmailAsync(It.IsAny<string>(), It.IsAny<bool>()))
+            .ReturnsAsync(new Soul());
+        _mockRepo.Setup(x => x.SoulRepository.IsMemberOfSpaceAsync(It.IsAny<Guid>(), It.IsAny<Guid>()))
+            .ReturnsAsync(true);
+        _mockSlugHelper.Setup(x => x.CreateSlug(It.IsAny<string>()))
+            .Returns("fake-title");
+        _mockRepo.Setup(x => x.SpaceRepository.CreateTopicAsync(It.IsAny<Topic>()))
+            .Callback<Topic>(x => createdTopic = new Topic
+            {
+                Id = Guid.NewGuid(),
+                Content = newTopic.Content,
+                SpaceId = newTopic.SpaceId,
+                SoulId = newTopic.SoulId,
+                CreatedBy = newTopic.CreatedBy,
+                CreatedDateUtc = newTopic.CreatedDateUtc
+            });
+
+        await space.CreateTopicAsync(authorEmail, newTopic.Title, newTopic.Content, _mockRepo.Object, _mockHelper.Object);
+
+        _mockRepo.Verify(x => x.SpaceRepository.CreateTopicAsync(It.IsAny<Topic>()), Times.Once);
+        Assert.NotNull(createdTopic);
+        Assert.NotEqual(Guid.Empty, createdTopic?.Id);
     }
 }
