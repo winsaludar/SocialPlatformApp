165ef1c0aed11e9f59f73c8e882be872aed943b3
winsaludar <erwinsaludar@gmail.com>
2022-08-28 20:27:05 +0800

    Added unit tests for get all souls service method

--- src/Services/Space/Tests/Space.UnitTests/Services/SpaceServiceTests.cs
+++ src/Services/Space/Tests/Space.UnitTests/Services/SpaceServiceTests.cs
@@ -1,5 +1,6 @@
 ﻿using Moq;
 using Space.Contracts;
+using Space.Domain.Entities;
 using Space.Domain.Helpers;
 using Space.Domain.Repositories;
 using Space.Services;
@@ -40,7 +41,7 @@ public class SpaceServiceTests
     }
 
     [Fact]
-    public async Task GetAllAsync_SpacesAreNotEmptyFromDatabase_ReturnsSpacesDto()
+    public async Task GetAllAsync_SpacesAreNotEmptyFromDatabase_ReturnsNotEmptySpacesDto()
     {
         _mockRepo.Setup(x => x.SpaceRepository.GetAllAsync(It.IsAny<bool>(), It.IsAny<bool>()))
             .ReturnsAsync(new List<DomainEntities.Space>()
@@ -53,6 +54,46 @@ public class SpaceServiceTests
         var result = await _spaceService.GetAllAsync();
 
         Assert.IsType<List<SpaceDto>>(result);
+        Assert.NotEmpty(result);
+        Assert.Equal(3, result.Count());
+    }
+
+    [Fact]
+    public async Task GetAllSoulsAsync_SoulsAreEmptyFromDatabase_ReturnsEmptySoulsDto()
+    {
+        Guid spaceId = Guid.NewGuid();
+
+        _mockRepo.Setup(x => x.SpaceRepository.GetByIdAsync(spaceId, It.IsAny<bool>(), It.IsAny<bool>()))
+            .ReturnsAsync(new DomainEntities.Space
+            {
+                Souls = new List<Soul>()
+            });
+
+        var result = await _spaceService.GetAllSoulsAsync(spaceId);
+
+        Assert.IsType<List<SoulDto>>(result);
+        Assert.Empty(result);
+    }
+
+    [Fact]
+    public async Task GetAllSoulsAsync_SoulsAreNotEmptyFromDatabase_ReturnsNotEmptySoulsDto()
+    {
+        Guid spaceId = Guid.NewGuid();
+
+        _mockRepo.Setup(x => x.SpaceRepository.GetByIdAsync(spaceId, It.IsAny<bool>(), It.IsAny<bool>()))
+            .ReturnsAsync(new DomainEntities.Space
+            {
+                Souls = new List<Soul>
+                {
+                    new Soul(),
+                    new Soul(),
+                    new Soul()
+                }
+            });
+
+        var result = await _spaceService.GetAllSoulsAsync(spaceId);
+
+        Assert.IsType<List<SoulDto>>(result);
         Assert.NotEmpty(result);
         Assert.Equal(3, result.Count());
     }
