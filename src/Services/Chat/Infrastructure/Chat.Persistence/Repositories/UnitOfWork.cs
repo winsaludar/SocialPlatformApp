3e026047a281fbf76c6ad1a69077359659f97aaf
winsaludar <erwinsaludar@gmail.com>
2022-09-17 00:30:44 +0800

    Added chat persistence project

---
+++ src/Services/Chat/Infrastructure/Chat.Persistence/Repositories/UnitOfWork.cs
@@ -0,0 +1,21 @@
+﻿using Chat.Domain.Repositories;
+
+namespace Chat.Persistence.Repositories;
+
+public class UnitOfWork : IUnitOfWork
+{
+    public Task BeginTransactionAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task CommitAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task RollbackAsync()
+    {
+        throw new NotImplementedException();
+    }
+}
