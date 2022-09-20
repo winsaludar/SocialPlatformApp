f17b773d90663a12665eba1c24ddd25fcbc92289
winsaludar <erwinsaludar@gmail.com>
2022-09-20 11:29:47 +0800

    Added servers controller without any functionality yet

---
+++ src/Services/Chat/Application/Chat.Presentation/Controllers/ServersController.cs
@@ -0,0 +1,23 @@
+﻿using Chat.Presentation.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Chat.Presentation.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+[Authorize]
+public class ServersController : ControllerBase
+{
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(string))]
+    [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(string))]
+    public async Task<IActionResult> PostAsync([FromBody] CreateServerRequest request)
+    {
+        if (!ModelState.IsValid)
+            return BadRequest("Please provide all the required fields");
+
+        return Ok("Created");
+    }
+}
