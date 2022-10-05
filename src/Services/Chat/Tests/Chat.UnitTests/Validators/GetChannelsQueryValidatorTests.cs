155eeb75e8ab83ca02db4ae0a72434d2ec9e2946
winsaludar <erwinsaludar@gmail.com>
2022-10-05 20:25:12 +0800

    Added unit tests for get channels query validator

---
+++ src/Services/Chat/Tests/Chat.UnitTests/Validators/GetChannelsQueryValidatorTests.cs
@@ -0,0 +1,50 @@
+﻿using Chat.Application.Queries;
+using Chat.Application.Validators;
+using Chat.Domain.Aggregates.ServerAggregate;
+using Chat.Domain.Exceptions;
+using Chat.Domain.SeedWork;
+using Moq;
+
+namespace Chat.UnitTests.Validators;
+
+public class GetChannelsQueryValidatorTests
+{
+    private readonly Mock<IRepositoryManager> _mockRepositoryManager;
+    private readonly GetChannelsQueryValidator _validator;
+
+    public GetChannelsQueryValidatorTests()
+    {
+        Mock<IServerRepository> mockServerRepository = new();
+        _mockRepositoryManager = new Mock<IRepositoryManager>();
+        _mockRepositoryManager.Setup(x => x.ServerRepository).Returns(mockServerRepository.Object);
+        _validator = new GetChannelsQueryValidator(_mockRepositoryManager.Object);
+    }
+
+    [Fact]
+    public async Task TargetServerId_IsEmpty_ReturnsError()
+    {
+        // Arrange
+        GetChannelsQuery query = new(Guid.Empty);
+        _mockRepositoryManager.Setup(x => x.ServerRepository.GetByIdAsync(It.IsAny<Guid>()))
+            .ReturnsAsync(new Server("Target Server", "Short Desc", "Long Desc", "creator@example.com", ""));
+
+        // Act
+        var result = await _validator.ValidateAsync(query, It.IsAny<CancellationToken>());
+
+        // Assert
+        Assert.NotEmpty(result.Errors);
+        Assert.True(result.Errors?.Any(x => x.PropertyName == "TargetServerId"));
+    }
+
+    [Fact]
+    public async Task TargetServerId_IsInvalid_ThrowsServerNotFoundException()
+    {
+        // Arrange
+        GetChannelsQuery query = new(Guid.NewGuid());
+        _mockRepositoryManager.Setup(x => x.ServerRepository.GetByIdAsync(It.IsAny<Guid>()))
+            .ReturnsAsync((Server)null!);
+
+        // Act & Assert
+        await Assert.ThrowsAsync<ServerNotFoundException>(() => _validator.ValidateAsync(query, It.IsAny<CancellationToken>()));
+    }
+}
