2d1e2a8d296a7b1787e03225b149ddf2952af10e
winsaludar <erwinsaludar@gmail.com>
2022-10-21 18:26:42 +0800

    Added unit test for remove moderator command handler

---
+++ src/Services/Chat/Tests/Chat.UnitTests/Commands/RemoveModeratorCommandHandlerTests.cs
@@ -0,0 +1,36 @@
+﻿using Chat.Application.Commands;
+using Chat.Domain.Aggregates.ServerAggregate;
+using Chat.Domain.SeedWork;
+using Moq;
+
+namespace Chat.UnitTests.Commands;
+
+public class RemoveModeratorCommandHandlerTests
+{
+    private readonly Mock<IRepositoryManager> _mockRepositoryManager;
+    private readonly RemoveModeratorCommandHandler _removeModeratorCommandHandler;
+
+    public RemoveModeratorCommandHandlerTests()
+    {
+        Mock<IServerRepository> mockServerRepository = new();
+        _mockRepositoryManager = new Mock<IRepositoryManager>();
+        _mockRepositoryManager.Setup(x => x.ServerRepository).Returns(mockServerRepository.Object);
+        _removeModeratorCommandHandler = new RemoveModeratorCommandHandler(_mockRepositoryManager.Object);
+    }
+
+    [Fact]
+    public async Task Handle_ModeratorAdded_ReturnsTrue()
+    {
+        // Arrange
+        Server targetServer = new("Target Server", "Short Desc", "Long Desc", "");
+        RemoveModeratorCommand command = new(targetServer, Guid.NewGuid(), Guid.NewGuid());
+        _mockRepositoryManager.Setup(x => x.ServerRepository.GetByIdAsync(It.IsAny<Guid>())).ReturnsAsync(targetServer);
+
+        // Act
+        var result = await _removeModeratorCommandHandler.Handle(command, It.IsAny<CancellationToken>());
+
+        // Assert
+        _mockRepositoryManager.Verify(x => x.ServerRepository.UpdateAsync(It.IsAny<Server>()), Times.Once);
+        Assert.True(result);
+    }
+}
