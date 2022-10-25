b9378b4e04f2ee137f751928ddc989a39d79522a
winsaludar <erwinsaludar@gmail.com>
2022-10-25 18:16:14 +0800

    Added unit tests for add channel member command validator

---
+++ src/Services/Chat/Tests/Chat.UnitTests/Validators/AddChannelMemberCommandValidatorTests.cs
@@ -0,0 +1,171 @@
+﻿using Chat.Application.Commands;
+using Chat.Application.Validators;
+using Chat.Domain.Aggregates.ServerAggregate;
+using Chat.Domain.Aggregates.UserAggregate;
+using Chat.Domain.Exceptions;
+using Chat.Domain.SeedWork;
+using Moq;
+
+namespace Chat.UnitTests.Validators;
+
+public class AddChannelMemberCommandValidatorTests
+{
+    private readonly Mock<IRepositoryManager> _mockRepositoryManager;
+    private readonly AddChannelMemberCommandValidator _validator;
+
+    public AddChannelMemberCommandValidatorTests()
+    {
+        Mock<IServerRepository> mockServerRepository = new();
+        Mock<IUserRepository> mockUserRepository = new();
+        _mockRepositoryManager = new Mock<IRepositoryManager>();
+        _mockRepositoryManager.Setup(x => x.ServerRepository).Returns(mockServerRepository.Object);
+        _mockRepositoryManager.Setup(x => x.UserRepository).Returns(mockUserRepository.Object);
+        _validator = new AddChannelMemberCommandValidator(_mockRepositoryManager.Object);
+    }
+
+    [Fact]
+    public async Task TargetServer_IsInvalid_ThrowsServerNotFoundException()
+    {
+        // Arrange
+        Server targetServer = GetTargetServer();
+        AddChannelMemberCommand command = new(targetServer, Guid.NewGuid(), Guid.NewGuid(), Guid.NewGuid());
+        _mockRepositoryManager.Setup(x => x.ServerRepository.GetByIdAsync(It.IsAny<Guid>())).ReturnsAsync((Server)null!);
+
+        // Act & Assert
+        await Assert.ThrowsAsync<ServerNotFoundException>(() => _validator.ValidateAsync(command, It.IsAny<CancellationToken>()));
+    }
+
+    [Fact]
+    public async Task ChannelId_IsEmpty_ReturnsAnError()
+    {
+        // Arrange
+        Guid userId = Guid.NewGuid();
+        Server targetServer = GetTargetServer();
+        targetServer.AddChannel(Guid.Empty, "notexisting", true, targetServer.CreatedById, DateTime.UtcNow);
+        targetServer.AddMember(userId, "user", DateTime.UtcNow);
+        AddChannelMemberCommand command = new(targetServer, Guid.Empty, userId, targetServer.CreatedById);
+        _mockRepositoryManager.Setup(x => x.ServerRepository.GetByIdAsync(It.IsAny<Guid>())).ReturnsAsync(targetServer);
+        _mockRepositoryManager.Setup(x => x.UserRepository.GetByIdAsync(It.IsAny<Guid>())).ReturnsAsync(GetUser(targetServer.CreatedById));
+
+        // Act
+        var result = await _validator.ValidateAsync(command, It.IsAny<CancellationToken>());
+
+        // Assert
+        Assert.NotEmpty(result.Errors);
+        Assert.True(result.Errors?.Any(x => x.PropertyName == "ChannelId"));
+    }
+
+    [Fact]
+    public async Task UserId_IsEmpty_ReturnsAnError()
+    {
+        // Arrange
+        Guid channelId = Guid.NewGuid();
+        Server targetServer = GetTargetServer();
+        targetServer.AddChannel(channelId, "channel", true, targetServer.CreatedById, DateTime.UtcNow);
+        targetServer.AddMember(Guid.Empty, "notexisting", DateTime.UtcNow);
+        AddChannelMemberCommand command = new(targetServer, channelId, Guid.Empty, targetServer.CreatedById);
+        _mockRepositoryManager.Setup(x => x.ServerRepository.GetByIdAsync(It.IsAny<Guid>())).ReturnsAsync(targetServer);
+        _mockRepositoryManager.Setup(x => x.UserRepository.GetByIdAsync(It.IsAny<Guid>())).ReturnsAsync(GetUser(targetServer.CreatedById));
+
+        // Act
+        var result = await _validator.ValidateAsync(command, It.IsAny<CancellationToken>());
+
+        // Assert
+        Assert.NotEmpty(result.Errors);
+        Assert.True(result.Errors?.Any(x => x.PropertyName == "UserId"));
+    }
+
+    [Fact]
+    public async Task UserId_IsInvalid_ThrowsUserNotFoundException()
+    {
+        // Arrange
+        Server targetServer = GetTargetServer();
+        AddChannelMemberCommand command = new(targetServer, Guid.NewGuid(), Guid.NewGuid(), targetServer.CreatedById);
+        _mockRepositoryManager.Setup(x => x.ServerRepository.GetByIdAsync(It.IsAny<Guid>())).ReturnsAsync(targetServer);
+        _mockRepositoryManager.Setup(x => x.UserRepository.GetByIdAsync(It.IsAny<Guid>())).ReturnsAsync((User)null!);
+
+        // Act & Assert
+        await Assert.ThrowsAsync<UserNotFoundException>(() => _validator.ValidateAsync(command, It.IsAny<CancellationToken>()));
+    }
+
+    [Fact]
+    public async Task AddedById_IsNotTheCreatorOrAModerator_ThrowsUnauthorizedUserException()
+    {
+        // Arrange
+        Guid channelId = Guid.NewGuid();
+        Guid userId = Guid.NewGuid();
+        Server targetServer = GetTargetServer();
+        AddChannelMemberCommand command = new(targetServer, channelId, userId, Guid.NewGuid());
+        _mockRepositoryManager.Setup(x => x.ServerRepository.GetByIdAsync(It.IsAny<Guid>())).ReturnsAsync(targetServer);
+        _mockRepositoryManager.Setup(x => x.UserRepository.GetByIdAsync(It.IsAny<Guid>())).ReturnsAsync(GetUser(Guid.NewGuid()));
+
+        // Act & Assert
+        await Assert.ThrowsAsync<UnauthorizedUserException>(() => _validator.ValidateAsync(command, It.IsAny<CancellationToken>()));
+    }
+
+    [Fact]
+    public async Task ChannelId_IsInvalid_ThrowsChannelNotFoundException()
+    {
+        // Arrange
+        Guid channelId = Guid.NewGuid();
+        Guid userId = Guid.NewGuid();
+        Server targetServer = GetTargetServer();
+        AddChannelMemberCommand command = new(targetServer, channelId, userId, targetServer.CreatedById);
+        _mockRepositoryManager.Setup(x => x.ServerRepository.GetByIdAsync(It.IsAny<Guid>())).ReturnsAsync(targetServer);
+        _mockRepositoryManager.Setup(x => x.UserRepository.GetByIdAsync(It.IsAny<Guid>())).ReturnsAsync(GetUser(targetServer.CreatedById));
+
+        // Act & Assert
+        await Assert.ThrowsAsync<ChannelNotFoundException>(() => _validator.ValidateAsync(command, It.IsAny<CancellationToken>()));
+    }
+
+    [Fact]
+    public async Task User_IsNotAMemberOfTheServer_ThrowsUserIsNotAMemberException()
+    {
+        // Arrange
+        Guid channelId = Guid.NewGuid();
+        Guid userId = Guid.NewGuid();
+        Server targetServer = GetTargetServer();
+        targetServer.AddChannel(channelId, "channel", true, targetServer.CreatedById, DateTime.UtcNow);
+        AddChannelMemberCommand command = new(targetServer, channelId, userId, targetServer.CreatedById);
+        _mockRepositoryManager.Setup(x => x.ServerRepository.GetByIdAsync(It.IsAny<Guid>())).ReturnsAsync(targetServer);
+        _mockRepositoryManager.Setup(x => x.UserRepository.GetByIdAsync(It.IsAny<Guid>())).ReturnsAsync(GetUser(targetServer.CreatedById));
+
+        // Act & Assert
+        await Assert.ThrowsAsync<UserIsNotAMemberException>(() => _validator.ValidateAsync(command, It.IsAny<CancellationToken>()));
+    }
+
+    [Fact]
+    public async Task User_IsAlreadyAMemberOfTheChannel_ThrowsUserIsAlreadyAMemberException()
+    {
+        // Arrange
+        Guid channelId = Guid.NewGuid();
+        Guid userId = Guid.NewGuid();
+        Server targetServer = GetTargetServer();
+        targetServer.AddMember(userId, "user", DateTime.UtcNow);
+        targetServer.AddChannel(channelId, "channel", true, targetServer.CreatedById, DateTime.UtcNow);
+        targetServer.AddChannelMember(channelId, userId);
+        AddChannelMemberCommand command = new(targetServer, channelId, userId, targetServer.CreatedById);
+        _mockRepositoryManager.Setup(x => x.ServerRepository.GetByIdAsync(It.IsAny<Guid>())).ReturnsAsync(targetServer);
+        _mockRepositoryManager.Setup(x => x.UserRepository.GetByIdAsync(It.IsAny<Guid>())).ReturnsAsync(GetUser(targetServer.CreatedById));
+
+        // Act & Assert
+        await Assert.ThrowsAsync<UserIsAlreadyAMemberException>(() => _validator.ValidateAsync(command, It.IsAny<CancellationToken>()));
+    }
+
+    private static Server GetTargetServer()
+    {
+        Server targetServer = new("Target Server", "Short Desc", "Long Desc", "creator@example.com", "");
+        targetServer.SetCreatedById(Guid.NewGuid());
+        targetServer.SetId(Guid.NewGuid());
+
+        return targetServer;
+    }
+
+    private static User GetUser(Guid userId)
+    {
+        User user = new(Guid.NewGuid(), "user", "user@example.com");
+        user.SetId(userId);
+
+        return user;
+    }
+}
