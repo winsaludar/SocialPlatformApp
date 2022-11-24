18e9b38820fd7b66a481af2afd0a8df486a5b823
winsaludar <erwinsaludar@gmail.com>
2022-11-24 23:26:58 +0800

    Added auto scroll to hero banner when item is click

--- client/webapp/components/app/AppContainer.js
+++ client/webapp/components/app/AppContainer.js
@@ -1,6 +1,6 @@
 import AppHero from "./AppHero";
 import AppList from "./AppList";
-import { useState } from "react";
+import { useState, useRef } from "react";
 import initialBanner from "../../public/images/placeholder/initial-banner.jpg";
 
 export default function AppContainer() {
@@ -10,14 +10,16 @@ export default function AppContainer() {
       "Donec eleifend erat lacus, ac porttitor purus ultrices at. Proin sagittis interdum ex in sollicitudin. Integer et nibh fringilla, vehicula velit quis, blandit lorem. Morbi id tortor ante. Quisque inest egestas, posuere magna feugiat, iaculis purus.",
     backgroundImage: initialBanner.src,
   });
+  const myHeaderRef = useRef(null);
 
   function handleItemClick(title, description, backgroundImage) {
     setHeroData({ title, description, backgroundImage });
+    myHeaderRef.current.scrollIntoView({ behavior: "smooth", block: "center" });
   }
 
   return (
     <>
-      <header>
+      <header ref={myHeaderRef}>
         <AppHero data={heroData} />
       </header>
 
