c1eaf83f53c37af3a98998556f231a4d8a8efc86
winsaludar <erwinsaludar@gmail.com>
2022-11-28 18:12:07 +0800

    Added link to chat app

--- client/webapp/src/components/Card.js
+++ client/webapp/src/components/Card.js
@@ -1,6 +1,8 @@
 import Image from "next/image";
+import utilStyles from "../../styles/utils.module.css";
 
 export default function AppListItem({
+  cssBacgroundImage,
   imageSrc,
   imageWidth,
   imageHeight,
@@ -11,15 +13,24 @@ export default function AppListItem({
   hasButton,
   buttonTitle,
   buttonOnClick,
+  isDisabled,
 }) {
   return (
     <div
-      className={styles.card}
-      onClick={() => onClick(title, description, imageSrc)}
+      className={`${styles.card} ${isDisabled ? utilStyles.cardDisabled : ""}`}
+      onClick={!isDisabled ? () => onClick(title, description, imageSrc) : null}
     >
-      <div className={styles.cardImage}>
-        <Image src={imageSrc} width={imageWidth} height={imageHeight} alt="" />
-      </div>
+      {imageSrc && imageWidth && imageHeight && (
+        <div className={styles.cardImage}>
+          <Image
+            src={imageSrc}
+            width={imageWidth}
+            height={imageHeight}
+            alt=""
+          />
+        </div>
+      )}
+
       <div className={styles.cardContent}>
         <h2 className={styles.cardTitle}>{title}</h2>
         <p className={styles.cardDescription}>{description}</p>
@@ -27,7 +38,15 @@ export default function AppListItem({
           <button
             type="button"
             className={styles.cardButton}
-            onClick={buttonOnClick}
+            onClick={
+              !isDisabled
+                ? (e) => {
+                    e.preventDefault();
+                    e.stopPropagation();
+                    buttonOnClick();
+                  }
+                : null
+            }
           >
             {buttonTitle}
           </button>
--- client/webapp/src/components/app/AppList.js
+++ client/webapp/src/components/app/AppList.js
@@ -1,11 +1,29 @@
 import styles from "../../../styles/AppComponent.module.css";
 import Card from "../Card";
+import { useRouter } from "next/router";
 
 export default function AppList({ onItemClick }) {
+  const route = useRouter(null);
+
   return (
     <>
       <div className={styles.grid}>
-        {[...Array(12)].map((x, i) => {
+        <Card
+          imageSrc={`/images/placeholder/ali-1.jpg`}
+          imageWidth={1680}
+          imageHeight={1050}
+          title={`Chat`}
+          description="Nullam posuere nibh augue, nec sagittis ex eleifend sed. Curabitur tristique porta consequat. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos."
+          onClick={onItemClick}
+          styles={styles}
+          hasButton={true}
+          buttonTitle="Open App"
+          buttonOnClick={() => {
+            route.push("/chat");
+          }}
+        />
+
+        {[...Array(11)].map((x, i) => {
           return (
             <Card
               key={i}
@@ -19,6 +37,7 @@ export default function AppList({ onItemClick }) {
               hasButton={true}
               buttonTitle="Open App"
               buttonOnClick={() => {}}
+              isDisabled={true}
             />
           );
         })}
