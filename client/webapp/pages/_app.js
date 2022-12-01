cc340c7e10ea28552131b2692fdfd6a53d303c6b
winsaludar <erwinsaludar@gmail.com>
2022-12-01 17:43:39 +0800

    Fixed issue on undefined page title

--- client/webapp/pages/_app.js
+++ client/webapp/pages/_app.js
@@ -1,14 +1,17 @@
 import { Analytics } from "@vercel/analytics/react";
+
 import Layout from "../src/components/Layout";
 
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
+    <>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
       <Analytics />
-    </Layout>
+    </>
   );
 }
 
